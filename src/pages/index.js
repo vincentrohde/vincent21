432904f0b57e12c0e322ad06eaa9949f9e9d42a2
Vincent Rohde <vincent.rohde@icloud.com>
2020-12-04 06:20:38 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
+import Button from '../components/Button/Button';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+          <Button>All works</Button>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
