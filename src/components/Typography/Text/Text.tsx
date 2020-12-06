86a33d8ecd8710145c5e1a657f484d1e9be91f48
Vincent Rohde <vincent.rohde@icloud.com>
2020-12-06 23:41:39 +0100

    update typography

--- src/components/Typography/Text/Text.tsx
+++ src/components/Typography/Text/Text.tsx
@@ -1,17 +1,8 @@
 import React, {ComponentProps} from 'react';
 import styles from './Text.module.scss';
-import classnames from 'classnames';
 
-interface Props extends ComponentProps<any> {
-    isWhite?: boolean;
-}
-
-const Text = ({isBlack = false, children}: Props) => {
-    return (
-        <p className={classnames(styles.Text, {[styles.isBlack]: isBlack})}>
-            {children && children}
-        </p>
-    );
-};
+const Text = ({children}: ComponentProps<any>) => (
+    <p className={styles.Text}>{children && children}</p>
+);
 
 export default Text;
