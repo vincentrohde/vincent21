65bb0c577a6b30d06a131afde3201f19335a25c1
Vincent Rohde <vincent.rohde@icloud.com>
2020-12-07 15:01:52 +0100

    add Contact section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,21 @@
-import Head from 'next/head';
-import styles from '../../styles/Home.module.css';
-import Button from '../components/Button/Button';
+import React from 'react';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Vincent Rohde</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+// Custom sections
 
-      <main className={styles.main}>
-          <Button>All works</Button>
-      </main>
+import Welcome from '../sections/Welcome/Welcome';
+import Works from '../sections/Works/Works';
+import Contact from '../sections/Contact/Contact';
+import Footer from '../sections/Footer/Footer';
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+const Home = () => {
+    return (
+        <div className={'app'}>
+            <Welcome />
+            <Works />
+            <Contact />
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/sections/Contact/Contact.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styles from './Contact.module.scss';
+
+// Custom components
+import Title from '../../components/Typography/Title/Title';
+import Text from '../../components/Typography/Text/Text';
+import Button from '../../components/Button/Button';
+
+const Contact = () => {
+    return (
+        <section className={styles.Contact}>
+            <div className={styles.titleContainer}>
+                <Title level={2}>Let&apos;s work together</Title>
+            </div>
+
+            <div className={styles.textContainer}>
+                <Text>
+                    By default, JSX doesn&apos;t let you render raw HTML from string values.
+                    However, there is a way to make it do that
+                </Text>
+            </div>
+
+            <div className={styles.mailContainer}>
+                <Title className={styles.subtitle} level={4}>
+                    Mail
+                </Title>
+                <div className={styles.addressContainer}>
+                    <Text className={styles.email}>mail@vincentroh.de</Text>
+                    <Button isLink={false} style={'black'}>
+                        Copy
+                    </Button>
+                </div>
+            </div>
+
+            <div className={styles.socialContainer}>
+                <Title className={styles.subtitle} level={4}>
+                    Socials
+                </Title>
+                <div className={styles.socialButtonContainer}>
+                    <Button className={styles.socialButton} style={'pink'}>
+                        Github
+                    </Button>
+                    <Button className={styles.socialButton} style={'pink'}>
+                        Twitter
+                    </Button>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
