f370f2d92e2945f551fda92a7738f829b94d783e
Vincent Rohde <vincent.rohde@icloud.com>
2020-12-11 21:31:21 +0100

    add hyphens toggle to List

--- src/components/Typography/List/List.tsx
+++ src/components/Typography/List/List.tsx
@@ -3,15 +3,15 @@ import styles from './List.module.scss';
 import classNames from 'classnames';
 
 // Components
-
 import Text from '../Text/Text';
 
 interface Props extends ComponentProps<any> {
     items: string[];
+    isHyphens?: boolean;
 }
 
-const List = ({items, className}: Props) => (
-    <ul className={classNames(styles.List, className)}>
+const List = ({items, isHyphens = true, className}: Props) => (
+    <ul className={classNames(styles.List, {[styles.isHyphens]: isHyphens}, className)}>
         {items.map((item, index) => (
             <li key={index}>
                 <Text>{item}</Text>
