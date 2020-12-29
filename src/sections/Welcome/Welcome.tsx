33ddf3f933e0db6627b7c645ece09e44943c4034
Vincent Rohde <vincent.rohde@icloud.com>
2020-12-29 14:08:22 +0100

    update Welcome section

--- src/sections/Welcome/Welcome.tsx
+++ src/sections/Welcome/Welcome.tsx
@@ -4,25 +4,23 @@ import styles from './Welcome.module.scss';
 // Custom components
 import Button from '../../components/Button/Button';
 import Title from '../../components/Typography/Title/Title';
-import Text from '../../components/Typography/Text/Text';
 import Logo from '../../components/Icons/Logo/Logo';
 
 const Welcome = () => {
     return (
         <div className={styles.Welcome}>
-            <Title level={1}>Vincent Rohde</Title>
-            <h2 className={styles.label}>JavaScript & Web Developer</h2>
-            <div className={styles.textContainer}>
-                <Text className={styles.introText}>
-                    I am a Berlin (Germany) based web-developer. Who specializes in building (and
-                    sometimes also designing) amazing applications for the internet.
-                </Text>
-            </div>
-            <div className={styles.buttonContainer}>
-                <Button style={'pink'}>Contact me</Button>
-            </div>
             <div className={styles.logoContainer}>
                 <Logo fill={styles.logoColor} />
+            </div>
+            <Title level={1} className={styles.heading}>
+                Hey, I am
+                <br className={styles.isMobileOnly} />
+                <span className={styles.isWhite}>Vincent Rohde.</span> A Berlin based{' '}
+                <span className={styles.isPink}>Web Developer</span> and sometimes{' '}
+                <span className={styles.isPink}>Designer.</span>
+            </Title>
+            <div className={styles.buttonContainer}>
+                <Button style={'pink'}>Contact me</Button>
             </div>
         </div>
     );
