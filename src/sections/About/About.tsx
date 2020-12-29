7123da2c8147b0229d73b224b4a964db7705029e
Vincent Rohde <vincent.rohde@icloud.com>
2020-12-29 15:40:50 +0100

    update About section

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -9,21 +9,26 @@ import Link from '../../components/Typography/Link/Link';
 
 const About = () => {
     return (
-        <div className={styles.About}>
-            <Title className={styles.title} level={2}>
-                Let me give you a short summary about myself :)
-            </Title>
-            <Text className={styles.text}>
-                Folly words widow one downs few age every seven. If miss part by fact he park just
-                shew. Discovered had get considered projection who favourable. Necessary up
-                knowledge it tolerably. Unwilling departure education is be dashwoods or an. Use off
-                agreeable law unwilling sir deficient curiosity instantly.
-            </Text>
-            <Link>My Resume</Link>
+        <section className={styles.About}>
             <div className={styles.imageContainer}>
                 <ProfilePicture />
             </div>
-        </div>
+            <div className={styles.message}>
+                <Title className={styles.title} level={2}>
+                    Let me give you a short summary about myself :)
+                </Title>
+                <Text className={styles.text}>
+                    Folly words widow one downs few age every seven. If miss part by fact he park
+                    just shew. Discovered had get considered projection who favourable. Necessary up
+                    knowledge it tolerably. Unwilling departure education is be dashwoods or an. Use
+                    off agreeable law unwilling sir deficient curiosity instantly.
+                </Text>
+                <Link color={'black'}>My Resume</Link>
+                <Title className={styles.time} level={4}>
+                    12:20
+                </Title>
+            </div>
+        </section>
     );
 };
 
