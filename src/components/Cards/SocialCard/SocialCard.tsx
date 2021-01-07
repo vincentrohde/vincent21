0c03f6af225622d0fcc06602a3137ef094f0f9d1
Vincent Rohde <vincent.rohde@icloud.com>
2021-01-07 20:17:04 +0100

    add SocialCard component

---
+++ src/components/Cards/SocialCard/SocialCard.tsx
@@ -0,0 +1,90 @@
+import React, {ComponentProps} from 'react';
+import styles from './SocialCard.module.scss';
+import classnames from 'classnames';
+
+// Components
+import ShadowCard from '../ShadowCard/ShadowCard';
+import OverlayImage from '../../OverlayImage/OverlayImage';
+import ProfilePicture from '../../ProfilePicture/ProfilePicture';
+import Title from '../../Typography/Title/Title';
+import Text from '../../Typography/Text/Text';
+import Button from '../../Button/Button';
+
+type SocialLink = {
+    name: string;
+    link: string;
+    icon?: string;
+};
+
+interface Props extends ComponentProps<any> {
+    name: string;
+    image: string;
+    location?: string;
+    mail?: string;
+    socials?: SocialLink[];
+}
+
+const SocialCard = ({name, image, location, mail, socials, className}: Props) => {
+    return (
+        <ShadowCard className={classnames(styles.SocialCard, className)} isWhite={true}>
+            <div className={styles.header}>
+                <OverlayImage
+                    className={styles.headerImage}
+                    src={image}
+                    overlay={styles.imageOverlay}
+                />
+                <div className={styles.socialAvatar}>
+                    <ProfilePicture isShadow shadowColor={'#7D65E8'} isZoom />
+                </div>
+            </div>
+            <div className={styles.content}>
+                <Title className={styles.center} level={4}>
+                    {name}
+                </Title>
+                {location && <Text className={styles.center}>{location}</Text>}
+
+                {mail && (
+                    <div className={styles.mailContainer}>
+                        <Title className={styles.subtitle} level={4}>
+                            Mail
+                        </Title>
+                        <div className={styles.addressContainer}>
+                            <Text className={styles.email}>{mail}</Text>
+                            <Button
+                                isLink={false}
+                                style={'black'}
+                                clickHandler={() => {
+                                    console.log('tets');
+                                }}
+                            >
+                                Copy
+                            </Button>
+                        </div>
+                    </div>
+                )}
+
+                {socials && socials.length && (
+                    <div className={styles.socialContainer}>
+                        <Title className={styles.subtitle} level={4}>
+                            Socials
+                        </Title>
+                        <div className={styles.socialButtonContainer}>
+                            {socials.map((social, index) => (
+                                <Button
+                                    key={index}
+                                    icon={social.icon}
+                                    href={social.link}
+                                    className={styles.socialButton}
+                                >
+                                    {social.name}
+                                </Button>
+                            ))}
+                        </div>
+                    </div>
+                )}
+            </div>
+        </ShadowCard>
+    );
+};
+
+export default SocialCard;
