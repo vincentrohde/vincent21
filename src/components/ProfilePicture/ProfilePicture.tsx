acf540db4fa70a0e3b7ec9c0e5072678574e211c
Vincent Rohde <vincent.rohde@icloud.com>
2021-01-07 20:15:30 +0100

    update ProfilePicture component

--- src/components/ProfilePicture/ProfilePicture.tsx
+++ src/components/ProfilePicture/ProfilePicture.tsx
@@ -2,12 +2,60 @@ import React from 'react';
 import styles from './ProfilePicture.module.scss';
 import classNames from 'classnames';
 
-const ProfilePicture = () => (
-    <img
-        className={classNames(styles.ProfilePicture, 'shadow')}
-        src={'./profile.jpg'}
-        alt="Picture of Vincent Rohde"
+// Custom components
+import ShadowBox from '../ShadowBox/ShadowBox';
+import Image from '../Image/Image';
+
+interface PictureProps {
+    isZoom?: boolean;
+}
+
+interface ShadowPictureProps {
+    shadowColor?: string;
+    isZoom?: PictureProps['isZoom'];
+}
+
+interface Props {
+    isShadow?: boolean;
+    shadowColor?: ShadowPictureProps['shadowColor'];
+    isZoom?: PictureProps['isZoom'];
+}
+
+const IMAGE_SRC = './profile.jpg';
+const IMAGE_ALT = 'Picture of Vincent Rohde';
+
+const Picture = ({isZoom}: PictureProps) => (
+    <Image
+        className={classNames(styles.ProfilePicture, {[styles.isZoom]: isZoom}, 'shadow')}
+        src={IMAGE_SRC}
+        alt={IMAGE_ALT}
     />
 );
+
+const ZoomPicture = ({isZoom}: PictureProps) =>
+    isZoom ? (
+        <div className={styles.imageWrapper}>
+            <Picture isZoom={isZoom} />
+        </div>
+    ) : (
+        <Picture isZoom={isZoom} />
+    );
+
+const ShadowPicture = ({shadowColor, isZoom}: ShadowPictureProps) => (
+    <ShadowBox shadowColor={shadowColor}>
+        <ZoomPicture isZoom={isZoom} />
+    </ShadowBox>
+);
+
+const ProfilePicture = ({isZoom = false, isShadow = false, shadowColor}: Props) =>
+    isShadow ? (
+        <>
+            <ShadowPicture isZoom={isZoom} shadowColor={shadowColor} />
+        </>
+    ) : (
+        <>
+            <ZoomPicture isZoom={isZoom} />
+        </>
+    );
 
 export default ProfilePicture;
