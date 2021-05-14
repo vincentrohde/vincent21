8a1466c4e635889cd58c4827cb6f7f507bdb8268
Vincent Rohde <vincent.rohde@icloud.com>
2021-05-14 20:29:48 +0200

    add ScrollItem to SocialCard

--- src/components/Cards/SocialCard/SocialCard.tsx
+++ src/components/Cards/SocialCard/SocialCard.tsx
@@ -10,6 +10,7 @@ import Title from '../../Typography/Title/Title';
 import Text from '../../Typography/Text/Text';
 import Button from '../../Button/Button';
 import ShadowBox from '../../ShadowBox/ShadowBox';
+import ScrollItem from '../../ScrollItem/ScrollItem';
 
 type SocialLink = {
     name: string;
@@ -31,62 +32,64 @@ const SocialCard = ({name, image, location, mail, socials, className}: Props) =>
     };
 
     return (
-        <ShadowCard className={classnames(styles.SocialCard, className)} isWhite={true}>
-            <div className={styles.header}>
-                <OverlayImage
-                    className={styles.headerImage}
-                    src={image}
-                    overlay={styles.imageOverlay}
-                />
-                <div className={styles.socialAvatar}>
-                    <ShadowBox shadowColor={'#7D65E8'} className={styles.avatarShadow}>
-                        <div className={styles.imageWrapper}>
-                            <ProfilePicture className={styles.avatarImg} />
-                        </div>
-                    </ShadowBox>
+        <ScrollItem>
+            <ShadowCard className={classnames(styles.SocialCard, className)} isWhite={true}>
+                <div className={styles.header}>
+                    <OverlayImage
+                        className={styles.headerImage}
+                        src={image}
+                        overlay={styles.imageOverlay}
+                    />
+                    <div className={styles.socialAvatar}>
+                        <ShadowBox shadowColor={'#7D65E8'} className={styles.avatarShadow}>
+                            <div className={styles.imageWrapper}>
+                                <ProfilePicture className={styles.avatarImg} />
+                            </div>
+                        </ShadowBox>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.content}>
-                <Title className={styles.center} level={4}>
-                    {name}
-                </Title>
-                {location && <Text className={styles.center}>{location}</Text>}
+                <div className={styles.content}>
+                    <Title className={styles.center} level={4}>
+                        {name}
+                    </Title>
+                    {location && <Text className={styles.center}>{location}</Text>}
 
-                {mail && (
-                    <div className={styles.mailContainer}>
-                        <Title className={styles.subtitle} level={4}>
-                            Mail
-                        </Title>
-                        <div className={styles.addressContainer}>
-                            <Text className={styles.email}>{mail}</Text>
-                            <Button isLink={false} style={'black'} clickHandler={onClick}>
-                                Copy
-                            </Button>
-                        </div>
-                    </div>
-                )}
-
-                {socials && socials.length && (
-                    <div className={styles.socialContainer}>
-                        <Title className={styles.subtitle} level={4}>
-                            Socials
-                        </Title>
-                        <div className={styles.socialButtonContainer}>
-                            {socials.map((social, index) => (
-                                <Button
-                                    key={index}
-                                    icon={social.icon}
-                                    href={social.link}
-                                    className={styles.socialButton}
-                                >
-                                    {social.name}
+                    {mail && (
+                        <div className={styles.mailContainer}>
+                            <Title className={styles.subtitle} level={4}>
+                                Mail
+                            </Title>
+                            <div className={styles.addressContainer}>
+                                <Text className={styles.email}>{mail}</Text>
+                                <Button isLink={false} style={'black'} clickHandler={onClick}>
+                                    Copy
                                 </Button>
-                            ))}
+                            </div>
                         </div>
-                    </div>
-                )}
-            </div>
-        </ShadowCard>
+                    )}
+
+                    {socials && socials.length && (
+                        <div className={styles.socialContainer}>
+                            <Title className={styles.subtitle} level={4}>
+                                Socials
+                            </Title>
+                            <div className={styles.socialButtonContainer}>
+                                {socials.map((social, index) => (
+                                    <Button
+                                        key={index}
+                                        icon={social.icon}
+                                        href={social.link}
+                                        className={styles.socialButton}
+                                    >
+                                        {social.name}
+                                    </Button>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </ShadowCard>
+        </ScrollItem>
     );
 };
 
