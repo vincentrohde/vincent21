d3dc5253878e4a7b6b86d5af32fc620e552a2465
Vincent Rohde <vincent.rohde@icloud.com>
2021-05-14 20:42:17 +0200

    add small styling updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,22 @@ import React from 'react';
 // Sections
 import Welcome from '../sections/Welcome/Welcome';
 import About from '../sections/About/About';
-import Employment from '../sections/Employment/Employment';
-import Works from '../sections/Works/Works';
-import Skills from '../sections/Skills/Skills';
+import Projects from '../sections/Projects/Projects';
+import Work from '../sections/Work/Work';
 import Contact from '../sections/Contact/Contact';
 import Footer from '../sections/Footer/Footer';
 
 const Home = () => {
     return (
-        <div className={'app'}>
-            <Welcome />
-            <About />
-            <Employment />
-            <Skills />
-            <Works />
-            <Contact />
-            <Footer />
+        <div className={'app-wrapper'}>
+            <div className={'app'}>
+                <Welcome />
+                <About />
+                <Projects />
+                <Work />
+                <Contact />
+                <Footer />
+            </div>
         </div>
     );
 };
